1e8dbd4385b9f46a7204d91b56ecb220360f8523
unknown <kerem.yky@gmail.com>
2026-01-05 16:53:28 +0300

    add amber border color, and disable state if not calculated yet

--- sw.js
+++ sw.js
@@ -1,15 +1,25 @@
-const CACHE_NAME = 'refuel-assist-v7.3';
+const CACHE_NAME = 'refuel-assist-v7.3.1';
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
